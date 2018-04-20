3b525bbbb1e873e11b6784f089e5b5a3779d6a74
pengfei.yuan <ypf9319@gmail.com>
2018-04-20 21:37:36 +0800

    delete some files

--- src/test/test.test.js
+++ src/test/test.test.js
@@ -1,4 +1,4 @@
-const { getJSONBySheet, getWorkSheets, writeJson2File } = require('../util');
+const { getJSONBySheet, getHTMLBySheet, getWorkSheets, writeJson2File, writeHTML2File } = require('../util');
 const path = require('path');
 const fileFullPath = path.join(__dirname, '../../public/uploads/0905.xlsx');
 
@@ -6,6 +6,12 @@ console.log('fileFullPath :', fileFullPath);
 
 const worksheet = getWorkSheets(fileFullPath);
 const { Sheets, SheetNames } = worksheet;
+
+SheetNames.forEach(n => {
+  let sheet = Sheets[n];
+  let html = getHTMLBySheet(sheet);
+  writeHTML2File(html, n);
+})
 
 
 
--- src/util.js
+++ src/util.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 
 const JSONFULLPATH = path.join(__dirname, '../public/downloads/data.json');
+const HTMLFULLPATH = path.join(__dirname, '../views/');
 
 const getWorkSheets = (fileFullPath) => {
   const { Sheets, SheetNames } = xlsx.readFile(fileFullPath);
@@ -14,6 +15,10 @@ const getJSONBySheet = (worksheet) => {
   return xlsx.utils.sheet_to_json(worksheet);
 }
 
+const getHTMLBySheet = (worksheet) => {
+  return xlsx.utils.sheet_to_html(worksheet);
+}
+
 const writeJson2File = (json, jsonFullPath = JSONFULLPATH) => {
   console.log('JSONFULLPATH :', jsonFullPath);
   fs.writeFile(jsonFullPath, JSON.stringify(json), (err) => {
@@ -21,10 +26,17 @@ const writeJson2File = (json, jsonFullPath = JSONFULLPATH) => {
   })
 }
 
+const writeHTML2File = (html, filename) => {
+  fs.writeFile(`${HTMLFULLPATH}${filename}.html`, html, (err) => {
+    if (err) throw new Error('generate json file failed');
+  })
+}
 
 
 module.exports = {
   getWorkSheets,
   getJSONBySheet,
-  writeJson2File
+  getHTMLBySheet,
+  writeJson2File,
+  writeHTML2File
 }
